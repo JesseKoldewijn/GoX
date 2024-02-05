df7d186f6185ec6f481b01a82c4ab4f41240d97b
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-02-05 23:03:37 +0100

    init: core setup w/ htmx example post to hard-swap dateTime string with updated one

---
+++ handlers/root.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+// GET /
+func HandleIndex(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	htmlIndex := `<html>
+					<head>
+						<title>GoX</title>
+					</head>
+					<body>
+						<div id="dynamic_value">` + now.String() + `</div>
+						<button hx-get="/dynamic" hx-trigger="click" hx-swap="outerHTML swap" hx-target="#dynamic_value">Get dynamic value</button>
+						<script src="https://unpkg.com/htmx.org@latest" defer></script>
+					</body>
+				</html>`
+
+	_, err := io.WriteString(w, htmlIndex)
+	if err != nil {
+		return
+	}
+	request := r
+	err = request.Body.Close()
+	if err != nil {
+		return
+	}
+}
+
+// GET /dynamic
+func HandleIndexDynamic(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	htmlIndexDynamic := "<div id=\"dynamic_value\">" + now.String() + "</div>"
+
+	_, err := io.WriteString(w, htmlIndexDynamic)
+	if err != nil {
+		return
+	}
+	request := r
+	err = request.Body.Close()
+	if err != nil {
+		return
+	}
+}
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+// To run:
+// go run root.go
+// Command-line options:
+//   -production : enables HTTPS on port 443
+//   -redirect-to-https : redirect HTTP to HTTTPS
+
+import (
+	"context"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"github.com/JesseKoldewijn/go-x/handlers"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+const (
+	httpPort = "127.0.0.1:8000"
+
+	// Note: change to your real host
+	allowedHost = "www.my-domain.com"
+)
+
+var (
+	flgProduction          = false
+	flgRedirectHTTPToHTTPS = false
+)
+
+func makeServerFromMux(mux *http.ServeMux) *http.Server {
+	// set timeouts so that a slow or malicious client doesn't
+	// hold resources forever
+	return &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      mux,
+	}
+}
+
+func makeHTTPServer() *http.Server {
+	mux := &http.ServeMux{}
+
+	routes := map[string]func(http.ResponseWriter, *http.Request){
+		"/":        handlers.HandleIndex,
+		"/dynamic": handlers.HandleIndexDynamic,
+	}
+
+	for path, handler := range routes {
+		mux.HandleFunc(path, handler)
+	}
+
+	return makeServerFromMux(mux)
+
+}
+
+func makeHTTPToHTTPSRedirectServer() *http.Server {
+	handleRedirect := func(w http.ResponseWriter, r *http.Request) {
+		newURI := "https://" + r.Host + r.URL.String()
+		http.Redirect(w, r, newURI, http.StatusFound)
+	}
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handleRedirect)
+	return makeServerFromMux(mux)
+}
+
+func parseFlags() {
+	flag.BoolVar(&flgProduction, "production", false, "if true, we start HTTPS server")
+	flag.BoolVar(&flgRedirectHTTPToHTTPS, "redirect-to-https", false, "if true, we redirect HTTP to HTTPS")
+	flag.Parse()
+}
+
+func main() {
+	parseFlags()
+	var m *autocert.Manager
+
+	var httpsSrv *http.Server
+
+	if flgProduction {
+		hostPolicy := func(ctx context.Context, host string) error {
+			// Note: change to your real host
+			if host == allowedHost {
+				return nil
+			}
+			return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
+		}
+
+		dataDir := "."
+		m = &autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: hostPolicy,
+			Cache:      autocert.DirCache(dataDir),
+		}
+
+		httpsSrv = makeHTTPServer()
+		httpsSrv.Addr = ":443"
+		httpsSrv.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
+
+		go func() {
+			fmt.Printf("Starting HTTPS server on %s\n", httpsSrv.Addr)
+			err := httpsSrv.ListenAndServeTLS("", "")
+			if err != nil {
+				log.Fatalf("httpsSrv.ListendAndServeTLS() failed with %s", err)
+			}
+		}()
+	}
+
+	var httpSrv *http.Server
+	if flgRedirectHTTPToHTTPS {
+		httpSrv = makeHTTPToHTTPSRedirectServer()
+	} else {
+		httpSrv = makeHTTPServer()
+	}
+	// allow autocert handle Let's Encrypt callbacks over http
+	if m != nil {
+		httpSrv.Handler = m.HTTPHandler(httpSrv.Handler)
+	}
+
+	httpSrv.Addr = httpPort
+	fmt.Printf("Starting HTTP server on %s\n", httpPort)
+	err := httpSrv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("httpSrv.ListenAndServe() failed with %s", err)
+	}
+}
